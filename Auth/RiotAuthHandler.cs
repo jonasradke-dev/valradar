e8b750594688f852442dd0ca66f9f1e7bf333c83
Jonas Radke <info@jonasradke.dev>
2026-05-22 19:35:32 +0200

    feat: implement RiotAuthHandler for handling authentication headers

---
+++ Auth/RiotAuthHandler.cs
@@ -0,0 +1,36 @@
+﻿using ValRadar.Models;
+
+namespace ValRadar.Auth;
+
+public class RiotAuthHandler : DelegatingHandler
+{
+    private readonly AuthState _authState;
+    private readonly string _clientPlatform;
+    private readonly Func<string> _clientVersionProvider;
+
+    public RiotAuthHandler(AuthState authState, string clientPlatform, Func<string> clientVersionProvider)
+    {
+        _authState = authState;
+        _clientPlatform = clientPlatform;
+        _clientVersionProvider = clientVersionProvider;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        if (request.RequestUri?.Host.EndsWith(".a.pvp.net") == true)
+        {
+            request.Headers.TryAddWithoutValidation(
+                "X-Riot-Entitlements-JWT", _authState.EntitlementToken);
+            request.Headers.TryAddWithoutValidation(
+                "Authorization", $"Bearer {_authState.AuthToken}");
+            request.Headers.TryAddWithoutValidation(
+                "X-Riot-ClientPlatform", _clientPlatform);
+            request.Headers.TryAddWithoutValidation(
+                "X-Riot-ClientVersion", _clientVersionProvider());
+
+        }
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
